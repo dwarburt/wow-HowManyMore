47074c73341f111753def26bfe76dd89a819465e
David Warburton <david.warburton@gmail.com>
2017-08-08 17:33:31 -0700

    Update with explicit arg setting

--- HowManyMore.lua
+++ HowManyMore.lua
@@ -2,10 +2,11 @@ function eventHandler_OnEvent(self, event, ...)
 	-- arg1
 	-- kill a mob = Snowfall Elk dies, you gain xxxx experience. (+889 exp Rested bonus)
 	-- turn in a quest = You gain xxx experience.
-	i,j = string.find(arg1, "%d+ experience.")
-	xp = string.sub(arg1, i, j)
-	xp = string.gsub(xp, " experience.", "")
-	xp_needed = UnitXPMax("player") - UnitXP("player")
+	arg1, argx = ...;
+	i,j = string.find(arg1, "%d+ experience.");
+	xp = string.sub(arg1, i, j);
+	xp = string.gsub(xp, " experience.", "");
+	xp_needed = UnitXPMax("player") - UnitXP("player");
 	hmm = xp_needed / xp;
 	print("HMM: Only about " .. ceil(hmm) .. " more!");
 end
